a813c3c194ae1aa9a43636488fcf0bff3123c55c
haraldurshoshan <haraldurshoshan@gmail.com>
2017-09-18 14:54:41 +0000

    add src

---
+++ src/main/java/is/ru/honn/Main.java
@@ -0,0 +1,17 @@
+package is.ru.honn;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    // write your code here
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = s.next();
+
+        System.out.println("name is: " + name);
+    }
+}
