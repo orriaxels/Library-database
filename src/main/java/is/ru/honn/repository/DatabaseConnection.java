a7ecde1fb2572f04c34ff318cdd81609e08e7af1
Orri Axelsson <orriaxels@gmail.com>
2017-09-19 16:32:44 +0000

    database connection

--- src/main/java/is/ru/honn/repository/DatabaseConnection.java
+++ src/main/java/is/ru/honn/repository/DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
 
     private Connection connect()
     {
-        String url = "jdbc:sqlite:test.db";
+        String url = "jdbc:sqlite:library.db";
         Connection conn = null;
 
         try {
@@ -21,15 +21,35 @@ public class DatabaseConnection {
         // db parameters
 
         // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+        String booksTable = "CREATE TABLE IF NOT EXISTS books (\n"
                 + " id integer PRIMARY KEY, \n"
-                + " name text NOT NULL, \n"
-                + " capacity real\n"
+                + " title text NOT NULL, \n"
+                + " fname text NOT NULL, \n"
+                + " lname text NOT NULL, \n"
+                + " published date, \n"
+                + " isbn text NOT NULL \n"
+                + ");";
+
+        String personTable = "CREATE TABLE IF NOT EXISTS persons (\n"
+                + " id integer PRIMARY KEY, \n"
+                + " fname text NOT NULL, \n"
+                + " lname text NOT NULL, \n"
+                + " email text NOT NULL, \n"
+                + " address text \n"
+                + ");";
+
+        String outloansTable = "CREATE TABLE IF NOT EXISTS outloans(\n"
+                + " pid integer, \n"
+                + " bid integer, \n"
+                + " dateOfLoan text NOT NULL, \n"
+                + " rating integer \n"
                 + ");";
 
         try(Connection conn = this.connect();
             Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
+            stmt.execute(booksTable);
+            stmt.execute(personTable);
+            stmt.execute(outloansTable);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
